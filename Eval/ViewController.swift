b8fc9f809da1ce774fc69f0108a1cd47affea176
Supinfo <supinfo@Supinfos-Mac.local>
2018-03-09 14:28:35 +0100

    Nettoyage code et un peu de couleur dans l'interface

--- Eval/ViewController.swift
+++ Eval/ViewController.swift
@@ -20,8 +20,6 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
     var data : [Any]? {
         didSet {
             DispatchQueue.main.async {
-//                self.tableView.isHidden = false
-//                self.wheel.stopAnimating()
                 self.tableView.reloadData()
             }
         }
@@ -36,8 +34,7 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "repliqueCell")
         
         let replique = self.data![indexPath.row] as! [String: String]
-//        print(replique["character"] as! String)
-//
+
         cell?.textLabel?.text = replique["title"]
         cell?.detailTextLabel?.text = "\(replique["character"]!) - \(replique["episode"]!)"
         
@@ -57,9 +54,18 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
         let request = URLRequest(url: url!)
         let session = URLSession.shared
         let task = session.dataTask(with: request) { (data, resp, err) in
+            
             Player.shared.playSound(data!)
         }
         task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if (indexPath.row % 2 == 0) {
+            cell.backgroundColor = UIColor(colorLiteralRed: 230/255, green: 151/255, blue: 95/255, alpha: 1)
+        } else {
+            cell.backgroundColor = .white
+        }
     }
     
     func initList() {
@@ -68,10 +74,6 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
         let url = URL(string: link)
         let request = URLRequest(url: url!)
         let session = URLSession.shared
-        
-        //        self.label.isHidden = true
-//        self.tableView.isHidden = true
-//        self.wheel.startAnimating()
         
         let task = session.dataTask(with: request) { (data, resp, err) in
             let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [Any]
@@ -91,16 +93,7 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
                 let d = $0 as! [String : String]
                 return (d["title"]?.lowercased().contains(recherche!.lowercased()))!
             })
-            tmp?.sorted(by: {
-                let d = $0 as! [String : String]
-                let d1 = $1 as! [String : String]
-                return d["title"]! < d1["title"]!
-            })
-            self.data = tmp?.sorted(by: {
-                let d = $0 as! [String : String]
-                let d1 = $1 as! [String : String]
-                return d["title"]! < d1["title"]!
-            })
+            self.data = mySort(tab: tmp, str: "title")
             break
         case 1:
             // Personnage
@@ -108,11 +101,7 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
                 let d = $0 as! [String : String]
                 return (d["character"]?.lowercased().contains(recherche!.lowercased()))!
             })
-            self.data = tmp?.sorted(by: {
-                let d = $0 as! [String : String]
-                let d1 = $1 as! [String : String]
-                return d["character"]! < d1["character"]!
-            })
+            self.data = mySort(tab: tmp, str: "character")
             break
         case 2:
             // Episode
@@ -120,11 +109,7 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
                 let d = $0 as! [String : String]
                 return (d["episode"]?.lowercased().contains(recherche!.lowercased()))!
             })
-            self.data = tmp?.sorted(by: {
-                let d = $0 as! [String : String]
-                let d1 = $1 as! [String : String]
-                return d["episode"]! < d1["episode"]!
-            })
+            self.data = mySort(tab: tmp, str: "episode")
             break
         default:
             break
@@ -140,32 +125,29 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
         switch(selectedScope) {
         case 0:
             // Titre
-            self.data = self.data?.sorted(by: {
-                let d = $0 as! [String : String]
-                let d1 = $1 as! [String : String]
-                return d["title"]! < d1["title"]!
-            })
-            break
+            self.data = mySort(tab: self.data, str: "title")
         case 1:
             // Personnage
-            self.data = self.data?.sorted(by: {
-                let d = $0 as! [String : String]
-                let d1 = $1 as! [String : String]
-                return d["character"]! < d1["character"]!
-            })
+            self.data = mySort(tab: self.data, str: "character")
             break
         case 2:
             // Episode
-            self.data = self.data?.sorted(by: {
-                let d = $0 as! [String : String]
-                let d1 = $1 as! [String : String]
-                return d["episode"]! < d1["episode"]!
-            })
+            self.data = mySort(tab: self.data, str: "episode")
             break
         default:
             break
         }
     }
 
+    // MARK: - My functions
+    
+    func mySort(tab: [Any]!, str: String) -> [Any] {
+        return (tab?.sorted(by: {
+            let d = $0 as! [String : String]
+            let d1 = $1 as! [String : String]
+            return d[str]! < d1[str]!
+        }))!
+    }
+    
 }
 
